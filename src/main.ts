68f7d8dd1b1598e15a9a25983824a923f487e6ab
FaresAhmed50 <fares.ahmed15967@aiet.edu.eg>
2025-06-02 22:59:17 +0300

    config syncfusion

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import { registerLicense } from '@syncfusion/ej2-base';
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NNaF5cXmBCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXlecnVQRGNfVkR+W0pWYUA=');
+
+
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
