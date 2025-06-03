7c9156fcd82683d4a28fa6aad6e8b79400ad5157
FaresAhmed50 <fares.ahmed15967@aiet.edu.eg>
2025-06-04 00:19:00 +0300

    Changing the enviroment key for syncfusion

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: false,
+  syncfusionKey: "Ngo9BigBOggjHTQxAR8/V1NNaF5cXmBCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXlecnVQRGNfVkR+W0pWYUA="
 };
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-
+import {environment} from './environments/environment'
 import { registerLicense } from '@syncfusion/ej2-base';
-registerLicense('Ngo9BigBOggjHTQxAR8/V1NNaF5cXmBCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXlecnVQRGNfVkR+W0pWYUA=');
 
 
+
+registerLicense(`${environment.syncfusionKey}`);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
