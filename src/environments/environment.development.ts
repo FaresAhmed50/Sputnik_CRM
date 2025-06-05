b303e8c3fff9acebf716c5a9e67d2d765af26ea9
FaresAhmed50 <fares.ahmed15967@aiet.edu.eg>
2025-06-05 21:15:25 +0300

    Adding the Enviroment Var

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  syncfusionKey: "Ngo9BigBOggjHTQxAR8/V1NNaF5cXmBCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXlecnVQRGNfVkR+W0pWYUA=",
+
+  auth : {
+    domain: 'dev-1dha8qt8zf60zyxc.us.auth0.com',
+    clientId: 'P64odwZiqNbiYRHPaQnMrh2bxyblICze',
+    authorizationParams: {
+      redirect_uri: 'http://localhost:4200/',
+      audience: 'https://dev-1dha8qt8zf60zyxc.us.auth0.com/api/v2/',
+      scope: 'openid profile email',
+    },
+  }
+};
