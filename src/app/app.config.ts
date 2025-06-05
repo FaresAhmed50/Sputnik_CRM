40434730cab40bba9f7af56df62d106c7ff2d3be
FaresAhmed50 <fares.ahmed15967@aiet.edu.eg>
2025-06-05 03:11:11 +0300

    Creating the Auth0 Providores

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,5 +5,20 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(),
+    provideAuth0({
+      domain : `${ENV.auth.domain}`,
+      clientId : `${ENV.auth.clientId}`,
+      authorizationParams : {
+        redirect_uri : 'http://localhost:4200',
+        audience : 'https://dev-1dha8qt8zf60zyxc.us.auth0.com/api/v2/',
+        scope : 'openid profile email',
+      },
+      useRefreshTokens : true,
+      cacheLocation : "localstorage",
+    })
+  ]
 };
