3e9e6ff2399758b61fee5bcd4d5d18232cb8346b
FaresAhmed50 <fares.ahmed15967@aiet.edu.eg>
2025-06-05 22:05:32 +0300

    changint the redirect url on production

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -6,7 +6,7 @@ export const environment = {
     domain: 'dev-1dha8qt8zf60zyxc.us.auth0.com',
     clientId: 'P64odwZiqNbiYRHPaQnMrh2bxyblICze',
     authorizationParams: {
-      redirect_uri: 'http://localhost:4200/',
+      redirect_uri: 'https://sputnik-crm.vercel.app/',
       audience: 'https://dev-1dha8qt8zf60zyxc.us.auth0.com/api/v2/',
       scope: 'openid profile email',
     },
